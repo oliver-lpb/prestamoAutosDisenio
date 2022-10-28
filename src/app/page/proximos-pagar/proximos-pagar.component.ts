de153bafd96e50fe6a4219e47b1547f4ed512f1d
oliver-lpb <82127783+oliver-lpb@users.noreply.github.com>
2022-10-28 09:56:05 -0600

    arreglo de reporte proximo a pagar

--- src/app/page/proximos-pagar/proximos-pagar.component.ts
+++ src/app/page/proximos-pagar/proximos-pagar.component.ts
@@ -36,10 +36,26 @@ export class ProximosPagarComponent implements OnInit {
   }
 
   fecha='';
+  fechaUno='';
+  fechaDos='';
+  fechaTres='';
+  fechaCuatro='';
+  fechaCinco='';
+  fechaSeis='';
+  fechaSiete='';
+  fechaOcho='';
 
   fechado(){
     let fechaHoy = new Date();
     this.fecha = (fechaHoy.getDate() + "/" + (fechaHoy.getMonth() + 1) + "/" + fechaHoy.getFullYear()) 
+    this.fechaUno = ((fechaHoy.getDate()+1) + "/" + (fechaHoy.getMonth() + 1) + "/" + fechaHoy.getFullYear())
+    this.fechaDos = ((fechaHoy.getDate()+2) + "/" + (fechaHoy.getMonth() + 1) + "/" + fechaHoy.getFullYear())
+    this.fechaTres = ((fechaHoy.getDate()+3) + "/" + (fechaHoy.getMonth() + 1) + "/" + fechaHoy.getFullYear())
+    this.fechaCuatro = ((fechaHoy.getDate()+4) + "/" + (fechaHoy.getMonth() + 1) + "/" + fechaHoy.getFullYear())
+    this.fechaCinco = ((fechaHoy.getDate()+5) + "/" + (fechaHoy.getMonth() + 1) + "/" + fechaHoy.getFullYear())
+    this.fechaSeis = ((fechaHoy.getDate()+6) + "/" + (fechaHoy.getMonth() + 1) + "/" + fechaHoy.getFullYear())
+    this.fechaSiete = ((fechaHoy.getDate()+7) + "/" + (fechaHoy.getMonth() + 1) + "/" + fechaHoy.getFullYear())
+    this.fechaOcho = ((fechaHoy.getDate()+8) + "/" + (fechaHoy.getMonth() + 1) + "/" + fechaHoy.getFullYear())
     console.log(this.fecha);
   }
   
@@ -48,7 +64,6 @@ export class ProximosPagarComponent implements OnInit {
     let nombreCliete='';
     let fotoCliente='';
     this.pagosHoy=[];
-
     this.data.getCotizacionDos().subscribe(doc=>{
       this.ventasGuadadas=[]
       doc.forEach((element:any)=>{
@@ -58,8 +73,6 @@ export class ProximosPagarComponent implements OnInit {
         })
       });
       //console.log(this.ventasGuadadas[0].pagos[1].fechaPago)
-
-    
         for(let i=0; i<this.ventasGuadadas.length; i++){
           //console.log(this.ventasGuadadas[i].idCliente)
           cliente = this.ventasGuadadas[i].idCliente;
@@ -85,7 +98,122 @@ export class ProximosPagarComponent implements OnInit {
                 fotoUrl: fotos || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE7O3jaT4a31_a-7EE_Szoo6wVE0TabQk7jogLziA&s",
               }
               )
+            }else if(this.fechaUno == this.ventasGuadadas[i].pagos[j].fechaPago){
+              console.log('este es el dia de pago');
+              //se selecciona el cliente
+              console.log(nombreCliete,'este es el nombre')
+              console.log(this.ventasGuadadas[i].pagos[j].fechaPago,'este es la fecha')
+              let descargaNombre = localStorage.getItem('nombre');
+              let fotos = localStorage.getItem('fotoUrl');
+              
+              this.pagosHoy.push({
+                nombre:  descargaNombre || "sin",
+                fecha: this.ventasGuadadas[i].pagos[j].fechaPago,
+                fotoUrl: fotos || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE7O3jaT4a31_a-7EE_Szoo6wVE0TabQk7jogLziA&s",
+              }
+              )
+            }else if(this.fechaDos == this.ventasGuadadas[i].pagos[j].fechaPago){
+              console.log('este es el dia de pago');
+              //se selecciona el cliente
+              console.log(nombreCliete,'este es el nombre')
+              console.log(this.ventasGuadadas[i].pagos[j].fechaPago,'este es la fecha')
+              let descargaNombre = localStorage.getItem('nombre');
+              let fotos = localStorage.getItem('fotoUrl');
+              
+              this.pagosHoy.push({
+                nombre:  descargaNombre || "sin",
+                fecha: this.ventasGuadadas[i].pagos[j].fechaPago,
+                fotoUrl: fotos || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE7O3jaT4a31_a-7EE_Szoo6wVE0TabQk7jogLziA&s",
+              }
+              )
+            }else if(this.fechaTres == this.ventasGuadadas[i].pagos[j].fechaPago){
+              console.log('este es el dia de pago');
+              //se selecciona el cliente
+              console.log(nombreCliete,'este es el nombre')
+              console.log(this.ventasGuadadas[i].pagos[j].fechaPago,'este es la fecha')
+              let descargaNombre = localStorage.getItem('nombre');
+              let fotos = localStorage.getItem('fotoUrl');
+              
+              this.pagosHoy.push({
+                nombre:  descargaNombre || "sin",
+                fecha: this.ventasGuadadas[i].pagos[j].fechaPago,
+                fotoUrl: fotos || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE7O3jaT4a31_a-7EE_Szoo6wVE0TabQk7jogLziA&s",
+              }
+              )
+            }else if(this.fechaCuatro == this.ventasGuadadas[i].pagos[j].fechaPago){
+              console.log('este es el dia de pago');
+              //se selecciona el cliente
+              console.log(nombreCliete,'este es el nombre')
+              console.log(this.ventasGuadadas[i].pagos[j].fechaPago,'este es la fecha')
+              let descargaNombre = localStorage.getItem('nombre');
+              let fotos = localStorage.getItem('fotoUrl');
+              
+              this.pagosHoy.push({
+                nombre:  descargaNombre || "sin",
+                fecha: this.ventasGuadadas[i].pagos[j].fechaPago,
+                fotoUrl: fotos || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE7O3jaT4a31_a-7EE_Szoo6wVE0TabQk7jogLziA&s",
+              }
+              )
+            }else if(this.fechaCinco == this.ventasGuadadas[i].pagos[j].fechaPago){
+              console.log('este es el dia de pago');
+              //se selecciona el cliente
+              console.log(nombreCliete,'este es el nombre')
+              console.log(this.ventasGuadadas[i].pagos[j].fechaPago,'este es la fecha')
+              let descargaNombre = localStorage.getItem('nombre');
+              let fotos = localStorage.getItem('fotoUrl');
+              
+              this.pagosHoy.push({
+                nombre:  descargaNombre || "sin",
+                fecha: this.ventasGuadadas[i].pagos[j].fechaPago,
+                fotoUrl: fotos || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE7O3jaT4a31_a-7EE_Szoo6wVE0TabQk7jogLziA&s",
+              }
+              )
+            }else if(this.fechaSeis == this.ventasGuadadas[i].pagos[j].fechaPago){
+              console.log('este es el dia de pago');
+              //se selecciona el cliente
+              console.log(nombreCliete,'este es el nombre')
+              console.log(this.ventasGuadadas[i].pagos[j].fechaPago,'este es la fecha')
+              let descargaNombre = localStorage.getItem('nombre');
+              let fotos = localStorage.getItem('fotoUrl');
+              
+              this.pagosHoy.push({
+                nombre:  descargaNombre || "sin",
+                fecha: this.ventasGuadadas[i].pagos[j].fechaPago,
+                fotoUrl: fotos || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE7O3jaT4a31_a-7EE_Szoo6wVE0TabQk7jogLziA&s",
+              }
+              )
+            }else if(this.fechaSiete == this.ventasGuadadas[i].pagos[j].fechaPago){
+              console.log('este es el dia de pago');
+              //se selecciona el cliente
+              console.log(nombreCliete,'este es el nombre')
+              console.log(this.ventasGuadadas[i].pagos[j].fechaPago,'este es la fecha')
+              let descargaNombre = localStorage.getItem('nombre');
+              let fotos = localStorage.getItem('fotoUrl');
+              
+              this.pagosHoy.push({
+                nombre:  descargaNombre || "sin",
+                fecha: this.ventasGuadadas[i].pagos[j].fechaPago,
+                fotoUrl: fotos || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE7O3jaT4a31_a-7EE_Szoo6wVE0TabQk7jogLziA&s",
+              }
+              )
+            }else if(this.fechaOcho == this.ventasGuadadas[i].pagos[j].fechaPago){
+              console.log('este es el dia de pago');
+              //se selecciona el cliente
+              console.log(nombreCliete,'este es el nombre')
+              console.log(this.ventasGuadadas[i].pagos[j].fechaPago,'este es la fecha')
+              let descargaNombre = localStorage.getItem('nombre');
+              let fotos = localStorage.getItem('fotoUrl');
+              
+              this.pagosHoy.push({
+                nombre:  descargaNombre || "sin",
+                fecha: this.ventasGuadadas[i].pagos[j].fechaPago,
+                fotoUrl: fotos || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE7O3jaT4a31_a-7EE_Szoo6wVE0TabQk7jogLziA&s",
+              }
+              )
             }
+
+
+
           }
         }
       
